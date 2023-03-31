e20effdff51341aa683ab31e3df76f1f94ca5aa4
Your Name <you@example.com>
2023-03-31 13:47:15 +0100

    added styling

--- src/App.js
+++ src/App.js
@@ -3,25 +3,38 @@ import React, { Component } from "react";
 ////REACT REDUX
 import { connect } from "react-redux";
 
+import "./App.css";
+
 class App extends Component {
   render() {
     return (
-      <>
-        {/* state is no longer used as the data is now in the store. we need to use PROPS. 
+      <div className="pageContainer">
+        <h1>Simple Counter:</h1>
+        <h2>My first react-redux project</h2>
+        <div className="gridContainer">
+          {/* state is no longer used as the data is now in the store. we need to use PROPS. 
         DISPATCH connects to the store and INCREMENT and DECREMENT are the reducer functions. */}
-        <button onClick={() => this.props.dispatch({ type: "INCREMENT" })}>
-          Add
-        </button>
-        <p>{this.props.count}</p>
-        <button onClick={() => this.props.dispatch({ type: "DECREMENT" })}>
-          Remove
-        </button>
-      </>
+          <button
+            className="btnAdd"
+            onClick={() => this.props.dispatch({ type: "INCREMENT" })}
+          >
+            ADD
+          </button>
+          <p>{this.props.count}</p>
+
+          <button
+            className="btnRemove"
+            onClick={() => this.props.dispatch({ type: "DECREMENT" })}
+          >
+            REMOVE
+          </button>
+        </div>
+      </div>
     );
   }
 }
 
-////Initial state (?)
+////Initial state
 function mapStateToProps(state) {
   return { count: state.count };
 }
